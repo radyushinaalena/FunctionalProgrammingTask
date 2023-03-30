e2e15c9ae443c84e92cb4b8aa50bfc9e5b638787
Alena <al.shel@bk.ru>
2023-03-31 01:25:26 +0300

    Задача на функциональное программирование

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст ");
+        String text = scanner.nextLine();
+        List<String> list = Arrays.asList(text.split("[-#$%^&!?,.0-9\\s]+"));
+        Map<String, Integer> frequency = list.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toMap(
+                        e -> e, e -> 1, Integer::sum
+                ));
+        Map<String, Integer> sortedMapDesc = frequency.entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .limit(10)
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1, LinkedHashMap::new));
+        System.out.println("В тексте " + list.size() + " слов(а)");
+        System.out.println("Топ 10: ");
+        sortedMapDesc.forEach((k, v) -> System.out.println(k + ": " + v));
     }
 }
